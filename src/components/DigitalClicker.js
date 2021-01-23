859b0709e063cf16724ff445cf208cc77e88095f
AbdelRaouf Dannaoui <123raoufdannaoui@gmail.com>
2021-01-23 11:50:26 +0000

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,23 @@
 // Code DigitalClicker Component Here
+import React from "react";
+
+class DigitalClicker extends React.Component{
+ constructor(){
+  super();
+  this.state = {
+    timesClicked:0,
+   };
+ }
+
+ adder = () => {
+   this.setState( prev => ({timesClicked: prev.timesClicked + 1})  )
+ }
+  render(){
+    return (
+      <div>
+        <button  onClick={this.adder}    >{this.state.timesClicked}</button>
+      </div>
+    )
+  }
+}
+export default DigitalClicker;
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,55 @@
 // Code YouTubeDebugger Component Here
+import React from 'react';
+
+class YouTubeDebugger extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      errors: [],
+      user: null,
+      settings: {
+        bitrate: 8,
+        video: {
+          resolution: '1080p'
+        }
+      }
+    };
+  }
+
+  changer1 = () => {
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        bitrate: 12
+      }
+    });
+  };
+
+  changer2 = () => {
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        video: {
+          ...this.state.settings.video,
+          resolution: '720p'
+        }
+      }
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <button className="bitrate" onClick={this.changer1}>
+           bitrate
+        </button>
+        <button className="resolution" onClick={this.changer2}>
+           resolution
+        </button>
+      </div>
+    );
+  }
+}
+
+export default YouTubeDebugger;
